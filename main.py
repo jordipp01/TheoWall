c98f3fc5289a6b4956068049c286690ea4f9f415
jordiperezpastor <100429127@alumnos.uc3m.es>
2022-10-17 19:54:29 +0200

    .gitignore y mejora de json

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import hashlib
 
 
 def nuevo_usuario():
+    nombre = input("Nombre: ")
+    apellidos = input("Apellidos: ")
+    usuario = input("Usuario: ")
     email = input("Email: ")
     pwd = input("Contraseña: ")
     pwd_b = bytes(pwd, 'utf-8')
@@ -10,15 +13,14 @@ def nuevo_usuario():
     hash = hashlib.sha256()
     hash.update(pwd_b)
     pwd_h = hash.hexdigest()
-    data = {"email": email, "password": str(pwd_h)}
+    data = {"nombre": nombre, apellidos: "apellidos", "usuario": usuario, "email": email, "password": str(pwd_h)}
     add_item(data)
     #with open(self._FILE_PATH, "w", encoding="utf-8", newline="") as file:
     #    json.dump(data, file, indent=2)
 
 
-
 def usuario():
-    u_email = input("Email: ")
+    u_usuario = input("Usuario: ")
     pwd = input("Contraseña: ")
     pwd_b = bytes(pwd, 'utf-8')
 
@@ -30,7 +32,7 @@ def usuario():
     i=0
     x = False
     for i in range(0,len(data_list)):
-        if u_email == data_list[i]["email"]:
+        if u_usuario == data_list[i]["usuario"]:
             x = True
             if u_pwd_h == data_list[i]["password"]:
                 print("Bienvenido")
@@ -43,9 +45,17 @@ def usuario():
 
 def load():
     """Loading data into the datalist"""
-    with open("data_file.json", "r", encoding="utf-8", newline="") as file:
-        data_list = json.load(file)
-        return data_list
+    try:
+        with open("data_file.json", "r", encoding="utf-8", newline="") as file:
+            data_list = json.load(file)
+            return data_list
+    except:
+        f = open("data_file.json", "w")
+        data = []
+        save(data)
+        return data
+
+
 
 
 def save(data_list):
@@ -77,7 +87,7 @@ while i != True:
     else:
         print("Input incorrecto\n")
         print("Si ya eres usuario, teclea [Y]")
-        print("para registrarte, teclea [N]")
+        print("Para registrarte, teclea [N]")
         yn = input()
 
 
