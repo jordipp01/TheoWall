f1c9e43e0fe599be33108f4da81ea1b8329c8117
jordiperezpastor <100429127@alumnos.uc3m.es>
2022-10-19 16:34:12 +0200

    modificar_credencial

--- main.py
+++ main.py
@@ -47,6 +47,7 @@ def gen_codigo(len=6):
 """
 
 def hash_pwd(pwd):
+    "Recibe la contraseña y devuelve el hash-SHA256"
     pwd_b = bytes(pwd, 'utf-8')
     hash = hashlib.sha256()
     hash.update(pwd_b)
@@ -55,7 +56,7 @@ def hash_pwd(pwd):
 
 
 def signin():
-    """Función: Crea un nuevo usuario de la app, genera hash de la contraseña e introduce sus datos en el archivo JSON"""
+    """Crea un nuevo usuario de la app"""
     nombre = input("Nombre: ")
     apellidos = input("Apellidos: ")
     usuario = input("Usuario: ")
@@ -82,7 +83,7 @@ def signin():
 
 
 def login():
-    """Función: Logearse. Pide usuario y contraseña y comprueba en el archivo JSON: usuario + hash_de_contraseña"""
+    """Logearse. Pide usuario y contraseña y comprueba en el archivo JSON: usuario + hash_de_contraseña"""
     u_usuario = input("Usuario: ")
     pwd = input("Contraseña: ")
     u_pwd_h = hash_pwd(pwd)
@@ -103,7 +104,7 @@ def login():
 
 ""
 def load():
-    """Carga los datos en data_list/data"""
+    """Cargar el JSON en data_list y devuelve data_list"""
     try:
         with open("data_file.json", "r", encoding="utf-8", newline="") as file:
             data_list = json.load(file)
@@ -115,6 +116,7 @@ def load():
         return data
 
 def imprimir_credenciales(user_data):
+    """Imprime todas las credenciales del usuario"""
     for i in range(len(user_data["contenido"])):
         print("id: " + user_data["contenido"][i]["id"])
         print("credencial: " + user_data["contenido"][i]["credencial"])
@@ -132,16 +134,19 @@ def add_item(item):
     save(data_list)
 
 def add_credential(cred, num_usuario):
+    """Añade una nueva credencial"""
     data_list = load()
     data_list[num_usuario]["contenido"].append(cred)
     save(data_list)
 
-def add_user(num_usuario, nw_value, item):
+def edit_user_field(num_usuario, nw_value, item):
+    """Modifica el campo del perfil del usuario"""
     data_list = load()
     data_list[num_usuario][item] = nw_value
     save(data_list)
 
 def del_credential(num_usuario, id_cred):
+    """Elimina una credencial existente"""
     data_list = load()
     x = False
     for i in range(0, len(data_list[num_usuario])):
@@ -154,106 +159,137 @@ def del_credential(num_usuario, id_cred):
         print("El id no existe")
 
 def modificar_usuario(item, num_usuario):
+    """Modifica el perfil de usuario"""
     if item == "nombre":
         nw_name = input("Nuevo nombre: ")
-        add_user(num_usuario, nw_name, item)
+        edit_user_field(num_usuario, nw_name, item)
 
     elif item == "apellidos":
         nw_ape = input("Nuevos apellidos: ")
-        add_user(num_usuario, nw_ape, item)
+        edit_user_field(num_usuario, nw_ape, item)
 
     elif item == "usuario":
         nw_us = input("Nuevo usuario: ")
-        add_user(num_usuario, nw_us, item)
+        edit_user_field(num_usuario, nw_us, item)
 
     elif item == "email":
         nw_email = input("Nuevo email: ")
-        add_user(num_usuario, nw_email, item)
+        edit_user_field(num_usuario, nw_email, item)
 
     elif item == "password":
         nw_pwd = input("Nueva contraseña: ")
         nw_pdw_h = hash_pwd(nw_pwd)
-        add_user(num_usuario, nw_pdw_h, item)
+        edit_user_field(num_usuario, nw_pdw_h, item)
 
 
 
-def modificar_credencial(id_mod):
-    pass
+def modificar_credencial(num_usuario, id_mod):
+    """Modifica una credencial existente"""
+    data_list = load()
+    credenciales = data_list[num_usuario]["contenido"]
+    found = False
+    print(credenciales)
+    for i in range(len(credenciales)):
+        if credenciales[i]["id"] == id_mod:
+            tipo = input("\n  Modificar id [1]"
+                          "\n  Modificar credencial [2]"
+                          "\n  Modificar todo [3]\n")
+            if tipo == "1":
+                nw_value =input("Nuevo id: ")
+                data_list[num_usuario]["contenido"][i]["id"] = nw_value
+            elif tipo == "2":
+                nw_value = input("Nueva credencial: ")
+                data_list[num_usuario]["contenido"][i]["credencial"] = nw_value
+            elif tipo == "3":
+                nw_value1 = input("New id: ")
+                data_list[num_usuario]["contenido"][i]["id"] = nw_value1
+                nw_value2 = input("Nueva credencial: ")
+                data_list[num_usuario]["contenido"][i]["credencial"] = nw_value2
+            found = True
+    if found == False:
+        print("No existe")
+    save(data_list)
 
+if __name__ == '__main__':
 
-print("Bienvenido a Theowall, tu gestor de contraseñas y documentos\n")
-print("Si ya eres usuario, teclea [Y]")
-print("para registrarte, teclea [N]")
-yn = input()
-x = False
-while x != True:
-    if yn == "y" or yn == "Y":
-        x = True
-        user_data, num_usuario = login()
-    elif yn == "n" or yn == "N":
-        x = True
-        signin()
-    else:
-        print("Input incorrecto\n")
-        print("Si ya eres usuario, teclea [Y]")
-        print("Para registrarte, teclea [N]")
-        yn = input()
-imprimir_credenciales(user_data)
+    print("Bienvenido a Theowall, tu gestor de contraseñas y documentos\n")
+    print("Si ya eres usuario, teclea [Y]")
+    print("para registrarte, teclea [N]")
+    yn = input()
+    x = False
+    while x != True:
+        if yn == "y" or yn == "Y":
+            x = True
+            user_data, num_usuario = login()
+        elif yn == "n" or yn == "N":
+            x = True
+            signin()
+        else:
+            print("Input incorrecto\n")
+            print("Si ya eres usuario, teclea [Y]")
+            print("Para registrarte, teclea [N]")
+            yn = input()
+    imprimir_credenciales(user_data)
 
-print("""Panel de control (selecciona una opción):  
+    print("""Panel de control (selecciona una opción):  
     Para editar una credencial -------- [1]
     Para crear una nueva credencial --- [2]
     Para eliminar una credencial ------ [3]
     Para editar perfil de usuario ----- [4]""")
-modo = input("Elección: ")
-modo = int(modo)
+    modo = input("Elección: ")
+    modo = int(modo)
 
-if modo == 1:
-    id_mod = input("Id de la credencial a modificar: ")
-    modificar_credencial(id_mod)
-elif modo == 2:
-    id_create = input("Id: ")
-    cred_create = input("Credencial: ")
-    data_create = {"id": id_create, "credencial": cred_create}
-    add_credential(data_create, num_usuario)
-elif modo == 3:
-    id_delete = input("Id: ")
-    del_credential(num_usuario, id_delete)
-elif modo == 4:
-    list_user = list(user_data.items())
-    for i in range(5):
-        print("\n" + list_user[i][0] + ": " + list_user[i][1])
-    item = input("¿Qué quiere cambiar?: ")
-    if item == "password":
-        pwd = input("Introduzca la contraseña anterior: ")
-        pwd_h = hash_pwd(pwd)
+    if modo == 1:
+        list_user = user_data["contenido"]
+        for i in range(len(list_user)):
+            print("\n        Id: " + list_user[i]["id"])
+            print("Credencial: " + list_user[i]["credencial"])
+        id_mod = input("Id de la credencial a modificar: ")
 
-        if pwd_h != list_user[4][1]:
-            print("Contraseña incorrecta")
+        modificar_credencial(num_usuario, id_mod)
+    elif modo == 2:
+        id_create = input("Id: ")
+        cred_create = input("Credencial: ")
+        data_create = {"id": id_create, "credencial": cred_create}
+        add_credential(data_create, num_usuario)
+    elif modo == 3:
+        id_delete = input("Id: ")
+        del_credential(num_usuario, id_delete)
+    elif modo == 4:
+        list_user = list(user_data.items())
+        for i in range(5):
+            print("\n" + list_user[i][0] + ": " + list_user[i][1])
+        item = input("¿Qué quiere cambiar?: ")
+        if item == "password":
+            pwd = input("Introduzca la contraseña anterior: ")
+            pwd_h = hash_pwd(pwd)
 
-    modificar_usuario(item, num_usuario)
+            if pwd_h != list_user[4][1]:
+                print("Contraseña incorrecta")
 
-else:
-    print("No existe")
+        modificar_usuario(item, num_usuario)
 
-
-
-
-
-
-
-"""
-@app.route('/data_file', methods = ['GET'])
-def get_data_file():
-    if request.method == 'GET':
-        # Mensaje impreso en la terminal de python
-        print("---------------- He recibido --------------------")
-        print("data_file")
-        print("-------------------------------------------------")
-        data_file = load()
-        return jsonify(data_file), 200
+    else:
+        print("No existe")
 
 
-app.run(host="0.0.0.0", port="5000")
-"""
+
+
+
+
+
+    """
+    @app.route('/data_file', methods = ['GET'])
+    def get_data_file():
+        if request.method == 'GET':
+            # Mensaje impreso en la terminal de python
+            print("---------------- He recibido --------------------")
+            print("data_file")
+            print("-------------------------------------------------")
+            data_file = load()
+            return jsonify(data_file), 200
+    
+    
+    app.run(host="0.0.0.0", port="5000")
+    """
 
